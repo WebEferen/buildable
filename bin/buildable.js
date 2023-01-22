1f038841729c57789f6c097fcce889cfc3b8635f
Michal Makowski <michal_makowski@external.mckinsey.com>
2023-01-22 19:51:33 +0100

    Commands cleanup

--- bin/buildable.js
+++ bin/buildable.js
@@ -1,16 +1,3 @@
 #!/usr/bin/env node
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-
-import run from '../lib/cli/run.js';
-import dependencyGraph from '../lib/cli/dependency-graph.js';
-import executionOrder from '../lib/cli/execution-order.js';
-
-yargs(hideBin(process.argv))
-    .command([run.name, run.alias], run.description, run.builder, run.handler)
-    .command([dependencyGraph.name, dependencyGraph.alias], dependencyGraph.description, dependencyGraph.builder, dependencyGraph.handler)
-    .command([executionOrder.name, executionOrder.alias], executionOrder.description, executionOrder.builder, executionOrder.handler)
-    .option('path', { alias: 'p', description: 'Relative path to packages directory', default: 'packages' })
-    .option('exclude', { alias: 'e', description: 'List of excluded packages', type: 'array' })
-    .option('config', { alias: 'c', description: 'Relative path to configuration file (TBD)', type: 'string' })
-    .parse();
+import cli from '../lib/cli/cli.js';
+cli.parse();
