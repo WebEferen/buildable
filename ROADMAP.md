bde86af65cffe36fdf0b7cfffa3e7f6cc31de17b
Michal Makowski <michal_makowski@external.mckinsey.com>
2023-01-22 18:41:10 +0100

    Switch to classes

--- ROADMAP.md
+++ ROADMAP.md
@@ -1,5 +1,4 @@
-# Roadmap for Buildable v1
+# Roadmap
 
-- [] Add custom logger (extract logic outside of modules)
-- [] Add dotenv and custom .file read for configuration (and add defaults)
-- [] Add readiness validators to support most of well known service listeners such as TSC, Nodemon, HapiJS, Express (or add this as the configuration)
+- Add custom logger (extract logic outside of modules)
+- Add readiness validators to support most of well known service listeners such as TSC, Nodemon, HapiJS, Express (or add this as the configuration)
--- bin/buildable.js
+++ bin/buildable.js
@@ -7,10 +7,10 @@ import dependencyGraph from '../lib/cli/dependency-graph.js';
 import executionOrder from '../lib/cli/execution-order.js';
 
 yargs(hideBin(process.argv))
-    .command(run.name, run.description, run.builder, run.handler)
-    .command(dependencyGraph.name, dependencyGraph.description, dependencyGraph.builder, dependencyGraph.handler)
-    .command(executionOrder.name, executionOrder.description, executionOrder.builder, executionOrder.handler)
-    .option('path', { alias: 'p', description: 'Relative path to packages directory (default: packages)', type: 'string' })
+    .command([run.name, run.alias], run.description, run.builder, run.handler)
+    .command([dependencyGraph.name, dependencyGraph.alias], dependencyGraph.description, dependencyGraph.builder, dependencyGraph.handler)
+    .command([executionOrder.name, executionOrder.alias], executionOrder.description, executionOrder.builder, executionOrder.handler)
+    .option('path', { alias: 'p', description: 'Relative path to packages directory', default: 'packages' })
     .option('exclude', { alias: 'e', description: 'List of excluded packages', type: 'array' })
     .option('config', { alias: 'c', description: 'Relative path to configuration file (TBD)', type: 'string' })
     .parse();
