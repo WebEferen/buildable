d77c6b9e9703647599b15b45b5738b2329c97f02
Michal Makowski <michal_makowski@external.mckinsey.com>
2023-01-24 20:47:27 +0100

    Moved update-notifier to bin

--- bin/buildable.js
+++ bin/buildable.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
+import update from "update-notifier";
 import cli from "../lib/cli/cli.js";
+import packageJson from "../package.json" assert { type: "json" };
+
+update({ pkg: packageJson }).notify();
+
 cli.parse();
