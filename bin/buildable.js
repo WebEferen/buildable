cadafab98bf79c2a02f3d58f73287ac29aa03e74
Michał Makowski <michal.makowski97@gmail.com>
2023-02-27 15:31:44 +0100

    Chore code cleanups (#18)
    * Cleanups
    
    Added pnpm install with workspaces

--- bin/buildable.js
+++ bin/buildable.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import update from "update-notifier";
 import cli from "../lib/cli/cli.js";
-import { readJsonFile } from "../lib/utils/read-file.js";
+import { readFile } from "../lib/utils/read-file.js";
 
-update({ pkg: readJsonFile("package.json", process.cwd()) }).notify();
+update({ pkg: readFile("package.json", process.cwd()) }).notify();
 cli.parse();
